33623841fcbed69de947c440f53a2da9cb289932
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-26 22:07:42 +0600

    17_2 Introduction To Conditionals

---
+++ comparisonJS.js
@@ -0,0 +1,35 @@
+// console.log(10 > 5) //true
+// console.log(10 < 5) //false
+// console.log(10 == 5); //false
+// console.log(10 <= 5) //false
+// console.log(10 >= 5); //true
+// console.log(10 != 5); //true
+//  
+
+// -----------------------
+
+var payeraPrice = 50;
+var applePrice = 250;
+// console.log(payeraPrice == applePrice);
+
+/**
+ * DIFFERENCE Between double equal or Triple Equal
+ * *************************
+ * == এবং ===
+ * *************************
+ */
+
+// console.log(10 == 10);
+// console.log(10 == '10'); //true // যখন double qual দেয়া হয় তখন শুধু মান টাকে চেক করে।
+// console.log(10 === '10'); //false // কিন্তু  যখন triple qual দেয়া হয় তখন মান টাকে এবং টাইপটাকে চেক করে।
+
+/**
+ * DIFFERENCE Between not equal or double not Equal
+ * *************************
+ * != এবং !==
+ * *************************
+ */
+
+
+// console.log(10 != '10'); //false // যখন not qual দেয়া হয় তখন শুধু মান টাকে চেক করে।
+// console.log(10 !== '10'); //true // কিন্তু যখন double not qual দেয়া হয় তখন মান টাকে এবং টাইপটাকে চেক করে।
