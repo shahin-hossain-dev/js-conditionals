e4466a6ef796af269feb2125be5d498791d40f43
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-26 23:05:17 +0600

    17_3 Introduction To If Else Condition

---
+++ if.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * if biriyani Price less then 500 taka
+ *      i will purchase it.
+ * 
+ * -------------STRUCTURE----------
+ * if(condition){
+ *  // কিছু একটা হবে।
+ * }
+ */
+
+const biriyaniPrice = 250;
+
+// যদি কনডিশন সত্য হয় তাহলে if ভিতরের জিনিসকে execute করবে, অন্যথায় করবে না।
+if (biriyaniPrice < 300) {
+    console.log('give me a plate of biriyani')
+}
+
+/**
+ * if..else condition
+ * 
+ * -----------STRUCTURE-----------------
+ * 
+ * if(condition){
+ *      execute code if the condition is true
+ * }
+ * else{
+ *      execute code if the condition if false
+ * }
+ * 
+ */
+
+var weight = 20;
+
+if (weight < 20) {
+    console.log('I will Carry out with myself');
+}
+else {
+    console.log('I will Rent a Rickshaw');
+}
