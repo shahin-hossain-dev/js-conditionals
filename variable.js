0485709ac65ff2006d8579fffa6542218cd35b4c
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-26 21:41:11 +0600

    17_1 Compare Variables And Comparison Operator

---
+++ variable.js
@@ -0,0 +1,35 @@
+/**
+ * VARIABLE ==> এটা একটা Container. যেখানে data store করে রাখা হয়।
+ */
+
+// number type 
+var sunGlassPrice = 80;
+//string 
+
+var subject = 'EEE';
+
+//boolean 
+var passed = true;
+
+/**
+ * Naming Convention
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. canton start with number 
+ * 5. can not be a keyword 
+ * 6. prefer camelCase
+ */
+
+// camelCase 
+var lastExamResult = 89;
+
+
+/**
+ * OPERATION 
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN 
+ * 4. null
+ * 5. Number
+ */
